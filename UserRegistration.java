b062d92443b825796cbbfdaf906046037eb5f1d6
sanket2498 <sanketdulange24@gmail.com>
2020-12-18 22:18:47 +0530

    Added : Mobile Number Validation

--- UserRegistration.java
+++ UserRegistration.java
@@ -6,7 +6,7 @@ public class UserRegistration
 
         public static void main(String[] args)
         {
-                String fname,lname,email;
+                String fname,lname,email,mobile;
                 Scanner sc=new Scanner(System.in);
                 System.out.println("Enter First Name:");
                 fname=sc.nextLine();
@@ -37,6 +37,16 @@ public class UserRegistration
 		else
 		{
 			System.out.println("Invalid Email");
+		}
+		System.out.println("Enter Mobile:");
+		mobile=sc.nextLine();
+		if(Pattern.matches("^[0-9]{2} [0-9]{10}",mobile))
+		{
+			System.out.println("Valid Mobile Number");
+		}
+		else
+		{
+			System.out.println("Invalid Mobile Number");
 		}
         }
 }
