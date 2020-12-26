d5299b2f4252a9c6b4951ff784b8a0f588b0b1e9
sanket2498 <sanketdulange24@gmail.com>
2020-12-26 09:33:46 +0530

    Added : JUnit Test to validate the User Entry

--- UserRegsitrationTest.java
+++ UserRegsitrationTest.java
@@ -7,36 +7,67 @@ import static org.junit.Assert.*;
 
 public class UserRegistrationTest {
 
-    @Test
-    public void nameValidation() {
-        UserRegsitration user=new UserRegistration();
-        String mood=user.NameValidation("Sanket");
+      @Test
+    public void givenFirstName_WhenProper_ShouldReturnHappy() {
+        main main0=new main();
+        String mood=main0.NameValidation("Sanket");
         Assert.assertThat(mood,CoreMatchers.is("HAPPY"));
     }
     @Test
-    public void lastNameValidation() {
-        UserRegsitration user=new UserRegistration();
-        String mood=user.LastNameValidation("Dulange");
-        Assert.assertThat(mood,CoreMatchers.is("HAPPY"));
-    }
-    @Test
-    public void emailValidation() {
-        UserRegsitration user=new UserRegistration();
-        String mood=user.EmailValidation("sanketdulange24@gmail.com");
-        Assert.assertThat(mood,CoreMatchers.is("HAPPY"));
+    public void givenFirstName_WhenProper_ShouldReturnSad() {
+        main main0=new main();
+        String mood=main0.NameValidation("Sa");
+        Assert.assertThat(mood,CoreMatchers.is("SAD"));
     }
 
     @Test
-    public void phoneValidation() {
-        UserRegsitration user=new UserRegistration();
-        String mood=user.PhoneValidation("91 9422484996");
+    public void givenLastName_WhenProper_ShouldReturnHappy() {
+        main main0=new main();
+        String mood=main0.LastNameValidation("Dulange");
         Assert.assertThat(mood,CoreMatchers.is("HAPPY"));
+    }
+    @Test
+    public void givenLastName_WhenProper_ShouldReturnSad() {
+        main main0=new main();
+        String mood=main0.LastNameValidation("Du");
+        Assert.assertThat(mood,CoreMatchers.is("SAD"));
+    }
+    @Test
+    public void givenEmail_WhenProper_ShouldReturnHappy() {
+        main main0=new main();
+        String mood=main0.EmailValidation("sanketdulange24@gmail.com");
+        Assert.assertThat(mood,CoreMatchers.is("HAPPY"));
+    }
+    @Test
+    public void givenEmail_WhenProper_ShouldReturnSad() {
+        main main0=new main();
+        String mood=main0.EmailValidation("sanketdulangegmail.com");
+        Assert.assertThat(mood,CoreMatchers.is("SAD"));
     }
 
     @Test
-    public void passwordValidation() {
-        UserRegsitration user=new UserRegistration();
-        String mood=user.PasswordValidation("Sanket@24");
+    public void givenPhone_WhenProper_ShouldReturnHappy(){
+        main main0=new main();
+        String mood=main0.PhoneValidation("91 9422484996");
         Assert.assertThat(mood,CoreMatchers.is("HAPPY"));
+    }
+    @Test
+    public void givenPhone_WhenProper_ShouldReturnSad(){
+        main main0=new main();
+        String mood=main0.PhoneValidation("919422484996");
+        Assert.assertThat(mood,CoreMatchers.is("SAD"));
+    }
+
+    @Test
+    public void givenPassword_WhenProper_ShouldReturnHappy() {
+        main main0=new main();
+        String mood=main0.PasswordValidation("Sanket@24");
+        Assert.assertThat(mood,CoreMatchers.is("HAPPY"));
+    }
+    @Test
+    public void givenPassword_WhenProper_ShouldReturnSad() {
+        main main0=new main();
+        String mood=main0.PasswordValidation("Sanket4");
+        Assert.assertThat(mood,CoreMatchers.is("SAD"));
     }
 }
